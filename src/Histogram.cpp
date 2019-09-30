a897bb5c0bd359d30456ab41b71bc50700b0b70c
Douglas Pontes <jdps@cin.ufpe.br>
2019-09-29 21:13:24 -0300

    Incrementando o debug

--- src/Histogram.cpp
+++ src/Histogram.cpp
@@ -47,9 +47,9 @@ cv::Mat Histogram::debug(){
 	for(int i = 0; i < tamanho;i++){
 		cv::rectangle(image,cv::Point((int)((i*DBG/tamanho)),(0*DBGY)+(DBGY*50)),cv::Point((int)((i+1)*DBG/tamanho),((DBGY*50)- hist[i]*DBGY)),cv::Scalar(255,0,255));
 		char nameAux[50];
-		sprintf(nameAux,"%d",1);
+		sprintf(nameAux,"%d",i*tamanho);
 		//terminar de colocar o texto
-		cv::putText(image,"funciona por favor",cv::Point(i,i),cv::FONT_HERSHEY_SIMPLEX,0.8,cv::Scalar(0,255,255),4);		
+		cv::putText(image,nameAux,cv::Point(i*DBG/tamanho,(DBGY*55)),cv::FONT_HERSHEY_SIMPLEX,0.3,cv::Scalar(255,255,255),1);		
 	}
 	return image;
 	
